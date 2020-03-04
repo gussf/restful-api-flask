acc2e86b42b1cd0f56bf1fb0f158bb9cc2189914
Gustavo Franco <gustavo.silva.franco1@gmail.com>
2020-03-04 19:51:51 -0300

    adding reqparse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from secret import auth, identity
@@ -9,20 +9,28 @@ app.secret_key = 'gustavo'
 api = Api(app)
 items = []
 
-jwt = JWT(app, authentication_handler=auth, identity_handler=identity) # /auth
+jwt = JWT(app, authentication_handler=auth, identity_handler=identity)  # /auth
+
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help='This field cannot be left blank'
+                        )
+
     @jwt_required()
     def get(self, name):
-        item =  next(filter(lambda x: x['name'] == name, items), None)
-        return {'item' : item}, 200 if item else 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': f'An item with name {name} already exists'}, 400
 
-        data = request.get_json()
-        item = {'name':data['name'], 'price': data['price']}
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
@@ -31,12 +39,14 @@ class Item(Resource):
         items = list(filter(lambda x: x['name'] != name, items))
         return '', 204
 
-
     def put(self, name):
-        data = request.get_json()
-        item = next(filter(lambda x: x['name'] != name, items), None)
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
         if item:
             item.update(data)
+        else:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
         return item
 
 
@@ -44,10 +54,9 @@ class ItemList(Resource):
     def get(self):
         return {'items': items}
 
+
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items/')
 
 
 app.run(port=5000, debug=True)
-
-
